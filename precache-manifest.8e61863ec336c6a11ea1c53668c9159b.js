a8fac4a0a9255432efe3d6b0b1e8cdfe60fa5b7b
gabrielgruter <gabriel.gruter@gmail.com>
2018-11-18 18:13:12 +0000

    Updates

---
+++ precache-manifest.8e61863ec336c6a11ea1c53668c9159b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "37a0c98adc4c15261f80ced29274e513",
+    "url": "/static/media/cv.37a0c98a.jpg"
+  },
+  {
+    "revision": "c1f304053142a668466a",
+    "url": "/static/css/main.1d041ea2.chunk.css"
+  },
+  {
+    "revision": "db8a6f0f3b684b89ebdf879c8ac93b1e",
+    "url": "/static/media/worldlylogowire.db8a6f0f.png"
+  },
+  {
+    "revision": "b3699856b762b5c9530d",
+    "url": "/static/js/1.b3699856.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6125806b53205ca4d12db01e59e7b7ea",
+    "url": "/static/media/about.6125806b.jpg"
+  },
+  {
+    "revision": "efa51c6b0fd33186636b6002a33f6484",
+    "url": "/static/media/portfolio.efa51c6b.jpg"
+  },
+  {
+    "revision": "c1f304053142a668466a",
+    "url": "/static/js/main.c1f30405.chunk.js"
+  },
+  {
+    "revision": "22c0be3f65cf9d739c6bd1c24da58f64",
+    "url": "/static/media/contact2.22c0be3f.jpg"
+  },
+  {
+    "revision": "36daf8384be1e28cd0077af3c1f9e287",
+    "url": "/static/media/worldlytitle.36daf838.png"
+  },
+  {
+    "revision": "f8a0415be08a61cdd81c16c4ab3fb7dc",
+    "url": "/static/media/worldlycolours.f8a0415b.png"
+  },
+  {
+    "revision": "3bfb5db4cb0dd330dd94713b45290ffc",
+    "url": "/static/media/worldlycomb.3bfb5db4.png"
+  },
+  {
+    "revision": "d484d2bb65a8cd0427c72c1103f31520",
+    "url": "/static/media/worldlylogo.d484d2bb.png"
+  },
+  {
+    "revision": "b3699856b762b5c9530d",
+    "url": "/static/css/1.3b049370.chunk.css"
+  },
+  {
+    "revision": "448f54e6e27e6f29ed8327fc05f993e4",
+    "url": "/index.html"
+  }
+];
