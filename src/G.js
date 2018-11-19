494b0b8890a972430931f4b49d8b343bfc95a502
gabrielgruter <gabriel.gruter@gmail.com>
2018-11-19 20:19:09 +0000

    switched portfolio render from react-skylight to react-router

--- src/G.js
+++ src/G.js
@@ -3,6 +3,7 @@ import FlexView from 'react-flexview'
 import { CarouselProvider, Slider, Slide, Dot } from 'pure-react-carousel'
 import 'pure-react-carousel/dist/react-carousel.es.css'
 import SkyLight from 'react-skylight'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ScrollArea from 'react-scrollbar'
 
 import ImageAbout from './res/about.jpg'
@@ -15,37 +16,64 @@ import WorldlyLogo from './res/portfolio/worldly/worldlylogo.png'
 import WorldlyWire from './res/portfolio/worldly/worldlylogowire.png'
 import WorldlyColour from './res/portfolio/worldly/worldlycolours.png'
 
+const buttonStripper = {
+    background: "none",
+    border: "none",
+    fontFamily: "Roboto",
+}
+const readMore = {
+    color: "var(--highlights)",
+    fontSize: "1vw",
+    fontFamily: "Roboto",
+    fontWeight: "300",
+    textDecoration: "none",
+    cursor: "pointer",
+}
+const dotStyle = {
+    color: "white",
+    fontSize: "2vw",
+    fontWeight: "100",
+}
+const emailStyle = {
+    fontSize: "2vw",
+    fontFamily: "Roboto",
+    margin: "0",
+}
+const portfolioStyle = {
+    width: "25vw",
+    height: "50vh",
+    marginLeft: "-12.5vw",
+    marginTop: "-25vh",
+}
+
+function PortfolioHome() {
+    return (
+        <section className="navSection">
+            <img src={ImagePortfolio} className="navImage" alt="portfolio" />
+            <section className="navText">
+                <FlexView column vAlignContent='center'>
+                    <a>Portfolio</a>
+                    <h1>DESIGN BEAUTIFULLY. <br /> DEVELOP EFFICIENTLY.</h1>
+                    <p>Not the average undergrad.<br />Extensive experience using Adobe Suite, React, Java, Visual Studio and more to carry a project all the way from conceptualisation to realisation.
+                    <br/>
+                    <Link to="/portfolio" style={readMore}>See Work...</Link></p>
+                </FlexView>
+            </section>
+        </section>
+    );
+}
+function PortfolioContent() {
+    return (
+        <section className="navSection">
+            <img src={ImageAbout} className="navImage" alt="aboutme" />
+            <img src={ImageAbout} className="navImage" alt="aboutme" />
+            <img src={ImageAbout} className="navImage" alt="aboutme" />
+        </section>
+    );
+}
+
 export default class G extends Component {
     render() {
-        const buttonStripper = {
-            background: "none",
-            border: "none",
-            fontFamily: "Roboto",
-        }
-        const readMore = {
-            color: "var(--highlights)",
-            fontSize: "1vw",
-            fontFamily: "Roboto",
-            fontWeight: "300",
-            textDecoration: "none",
-            cursor: "pointer",
-        }
-        const dotStyle = {
-            color: "white",
-            fontSize: "2vw",
-            fontWeight: "100",
-        }
-        const emailStyle = {
-            fontSize: "2vw",
-            fontFamily: "Roboto",
-            margin: "0",
-        }
-        const portfolioStyle = {
-            width: "25vw",
-            height: "50vh",
-            marginLeft: "-12.5vw",
-            marginTop: "-25vh",
-        }
         return (
             <div id="page">
                 <CarouselProvider
@@ -71,16 +99,12 @@ export default class G extends Component {
                                     </section>
                                 </Slide>
                                 <Slide index={1}>
-                                    <section className="navSection">
-                                        <img src={ImagePortfolio} className="navImage" alt="portfolio" />
-                                        <section className="navText">
-                                            <FlexView column vAlignContent='center'>
-                                                <a>Portfolio</a>
-                                                <h1>DESIGN BEAUTIFULLY. <br /> DEVELOP EFFICIENTLY.</h1>
-                                                <p>Not the average undergrad.<br />Extensive experience using Adobe Suite, React, Java, Visual Studio and more to carry a project all the way from conceptualisation to realisation.<button style={Object.assign({}, buttonStripper, readMore)} onClick={() => this.portfolioPop.show()}>(see work)</button></p>
-                                            </FlexView>
-                                        </section>
-                                    </section>
+                                    <Router>
+                                        <div>
+                                        <Route exact path="/" component={PortfolioHome} />
+                                        <Route path="/portfolio" component={PortfolioContent} />
+                                        </div>
+                                    </Router>
                                 </Slide>
                                 <Slide index={2}>
                                     <section className="navSection">
@@ -108,8 +132,6 @@ export default class G extends Component {
                                 </Slide>
                             </Slider>
                         </div>
-                        <div id="highlightBorder"></div>
-                        <div id="whiteBorder"></div>
                         <FlexView height='38vw' hAlignContent='center' vAlignContent='bottom'>
                             <section id="navBar">
                                 <Dot slide={0} style={Object.assign({}, dotStyle, buttonStripper)}><a>About Me</a></Dot>
@@ -119,25 +141,21 @@ export default class G extends Component {
                                 <Dot slide={2} style={Object.assign({}, dotStyle, buttonStripper)}><a>Qualifications &amp; CV</a></Dot>
                                 <a>&ensp;|&ensp;</a>
                                 <Dot slide={3} style={Object.assign({}, dotStyle, buttonStripper)}><a>Contact</a></Dot>
-                                <button style={buttonStripper} onClick={() => this.specThanks.show()}><a className="specialThanks">Special Thanks</a></button>
                             </section>
                         </FlexView>
+                        <div id="highlightBorder"></div>
+                        <div id="whiteBorder"></div>
                     </FlexView>
                 </CarouselProvider>
-                <SkyLight
-                    hideOnOverlayClicked ref={ref => this.specThanks = ref}
-                    dialogStyles={portfolioStyle}>
-                    <section>
-                        <h1 style={{fontSize:"1vw"}}>This website would not have been possible without:</h1>
-                        <p style={{fontSize:"0.8vw"}}>React.js<br /><a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">https://reactjs.org/</a></p>
-                        <p style={{fontSize:"0.8vw"}}>npm<br /><a href="https://www.npmjs.com/" target="_blank" rel="noopener noreferrer">https://www.npmjs.com/</a></p>
-                        <p style={{fontSize:"0.8vw"}}>FlexView<br /><a href="https://github.com/buildo/react-flexview" target="_blank" rel="noopener noreferrer">https://github.com/buildo/react-flexview</a></p>
-                        <p style={{fontSize:"0.8vw"}}>Pure React Carousel<br /><a href="https://www.npmjs.com/package/pure-react-carousel" target="_blank" rel="noopener noreferrer">https://www.npmjs.com/package/pure-react-carousel</a></p>
-                        <p style={{fontSize:"0.8vw"}}>SkyLight<br /><a href="https://marcio.github.io/react-skylight/" target="_blank" rel="noopener noreferrer">https://marcio.github.io/react-skylight/</a></p>
-                        <p style={{fontSize:"0.8vw"}}>Scrollbar<br /><a href="https://www.npmjs.com/package/react-scrollbar" target="_blank" rel="noopener noreferrer">https://www.npmjs.com/package/react-scrollbar</a></p>
-                    </section>
-                </SkyLight>
-            </div>
+            </div >
         )
     }
 }
+// This website would not have been possible without:
+// React.js https://reactjs.org/
+// npm https://www.npmjs.com/
+//
+// FlexView https://github.com/buildo/react-flexview
+// Pure React Carousel https://www.npmjs.com/package/pure-react-carousel
+// SkyLight https://marcio.github.io/react-skylight/
+// Scrollbar https://www.npmjs.com/package/react-scrollbar
