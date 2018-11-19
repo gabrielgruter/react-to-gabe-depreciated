89eafa9bc06779eb668deaf66cbf856ba5bbb6f6
gabrielgruter <gabriel.gruter@gmail.com>
2018-11-19 17:50:17 +0000

    expanded portfolio-worldly

--- src/G.js
+++ src/G.js
@@ -47,65 +47,65 @@ export default class G extends Component {
             marginLeft: "-37.5vw",
             marginTop: "-50vh",
         }
-        return(
+        return (
             <div id="page">
                 <CarouselProvider
-                        naturalSlideWidth={200}
-                        naturalSlideHeight={250}
-                        totalSlides={4}>
+                    naturalSlideWidth={200}
+                    naturalSlideHeight={250}
+                    totalSlides={4}>
                     <FlexView
-                            height='100vh'
-                            hAlignContent='center'
-                            vAlignContent='center'>
+                        height='100vh'
+                        hAlignContent='center'
+                        vAlignContent='center'>
                         <div id="navBack">
                             <Slider id="carousel">
                                 <Slide index={0}>
                                     <section className="navSection">
-                                    <img src={ImageAbout} className="navImage" alt="aboutme"/>
-                                    <section className="navText">
-                                        <FlexView column vAlignContent='center'>
-                                            <a>Gabriel Gruter</a>
-                                            <h1>INTERNATIONAL. <br/> MULTI-CULTURAL.</h1>
-                                            <p>Not the average undergrad.<br/>Fluency in English, German and Portuguese with British, Euro and Latin American nationalities. I can correspond with a variety of different clients and work in EU and non-EU countries.</p>
-                                        </FlexView>
+                                        <img src={ImageAbout} className="navImage" alt="aboutme" />
+                                        <section className="navText">
+                                            <FlexView column vAlignContent='center'>
+                                                <a>Gabriel Gruter</a>
+                                                <h1>INTERNATIONAL. <br /> MULTI-CULTURAL.</h1>
+                                                <p>Not the average undergrad.<br />Fluency in English, German and Portuguese with British, Euro and Latin American nationalities. I can correspond with a variety of different clients and work in EU and non-EU countries.</p>
+                                            </FlexView>
+                                        </section>
                                     </section>
-                                </section>
                                 </Slide>
                                 <Slide index={1}>
                                     <section className="navSection">
-                                    <img src={ImagePortfolio} className="navImage" alt="portfolio"/>
-                                    <section className="navText">
-                                        <FlexView column vAlignContent='center'>
-                                            <a>Portfolio</a>
-                                            <h1>DESIGN BEAUTIFULLY. <br/> DEVELOP EFFICIENTLY.</h1>
-                                            <p>Not the average undergrad.<br/>Extensive experience using Adobe Suite, React, Java, Visual Studio and more to carry a project all the way from conceptualisation to realisation.<button style={Object.assign({}, buttonStripper, readMore)} onClick={() => this.portfolioPop.show()}>(see work)</button></p>
-                                        </FlexView>
+                                        <img src={ImagePortfolio} className="navImage" alt="portfolio" />
+                                        <section className="navText">
+                                            <FlexView column vAlignContent='center'>
+                                                <a>Portfolio</a>
+                                                <h1>DESIGN BEAUTIFULLY. <br /> DEVELOP EFFICIENTLY.</h1>
+                                                <p>Not the average undergrad.<br />Extensive experience using Adobe Suite, React, Java, Visual Studio and more to carry a project all the way from conceptualisation to realisation.<button style={Object.assign({}, buttonStripper, readMore)} onClick={() => this.portfolioPop.show()}>(see work)</button></p>
+                                            </FlexView>
+                                        </section>
                                     </section>
-                                </section>
                                 </Slide>
                                 <Slide index={2}>
                                     <section className="navSection">
-                                    <img src={ImageCV} className="navImage" alt="CV"/>
-                                    <section className="navText">
-                                        <FlexView column vAlignContent='center'>
-                                            <a>Qualifications &amp; CV</a>
-                                            <h1>ADOBE SUITE. <br/> REACT, NODE, ANDROID, iOS.</h1>
-                                            <p>Not the average undergrad.<br/>1 year of industry experience with yvySolutions. Creating, working on and maintaining android/iOS apps as a web/app developer alongside my undergradude studies.<a style={readMore} href="/CV.pdf" target="_blank"> read more...</a></p>
-                                        </FlexView>
+                                        <img src={ImageCV} className="navImage" alt="CV" />
+                                        <section className="navText">
+                                            <FlexView column vAlignContent='center'>
+                                                <a>Qualifications &amp; CV</a>
+                                                <h1>ADOBE SUITE. <br /> REACT, NODE, ANDROID, iOS.</h1>
+                                                <p>Not the average undergrad.<br />1 year of industry experience with yvySolutions. Creating, working on and maintaining android/iOS apps as a web/app developer alongside my undergradude studies.<a style={readMore} href="/CV.pdf" target="_blank"> read more...</a></p>
+                                            </FlexView>
+                                        </section>
                                     </section>
-                                </section>
                                 </Slide>
                                 <Slide index={3}>
                                     <section className="navSection">
-                                    <img src={ImageContact} className="navImage" alt="aboutme"/>
-                                    <section className="navText">
-                                        <FlexView column vAlignContent='center'>
-                                            <a>Contact</a>
-                                            <h1>FAST. <br/> RESPONSIVE.</h1>
-                                            <p>Feel free to contact me anytime at:<br/><a href="mailto:contact@gabrielgruter.com" style={emailStyle}>contact@gabrielgruter.com</a></p>
-                                        </FlexView>
+                                        <img src={ImageContact} className="navImage" alt="aboutme" />
+                                        <section className="navText">
+                                            <FlexView column vAlignContent='center'>
+                                                <a>Contact</a>
+                                                <h1>FAST. <br /> RESPONSIVE.</h1>
+                                                <p>Feel free to contact me anytime at:<br /><a href="mailto:contact@gabrielgruter.com" style={emailStyle}>contact@gabrielgruter.com</a></p>
+                                            </FlexView>
+                                        </section>
                                     </section>
-                                </section>
                                 </Slide>
                             </Slider>
                         </div>
@@ -126,56 +126,68 @@ export default class G extends Component {
                     </FlexView>
                 </CarouselProvider>
                 <CarouselProvider
-                                naturalSlideWidth={1000}
-                                naturalSlideHeight={1000}
-                                totalSlides={2}>
+                    naturalSlideWidth={1000}
+                    naturalSlideHeight={1000}
+                    totalSlides={2}>
                     <SkyLight hideOnOverlayClicked dialogStyles={portfolioStyle} ref={ref => this.portfolioPop = ref}>
                         <Dot slide={0}><a>Page 1</a></Dot>
                         <Dot slide={1}><a>Worldy</a></Dot>
-                                <Slider>
-                                    <Slide index={0}>
-                                        <p>Page 1</p>
-                                </Slide>
-                                <Slide index={1}>
-                                    <FlexView column
-                                            hAlignContent="center"
-                                            height="100vh">
-                                        <img src={WorldlyTitle} style={{width: "25vw"}} alt="Worldly"/>
-                                        <ScrollArea className="portfolioBox" horizontal="false">
-                                            <FlexView 
-                                                    column 
-                                                    hAlignContent="left"
-                                                    marginRight="10vw"
-                                                    marginLeft="10vw">
-                                                <h2>Main Objective:</h2>
-                                                <p>To design a mobile news application aimed at a younger demographic (age 18 – 25).</p>
-                                                <h2>Key Features</h2>
-                                                <ul>
-                                                    <li>Gameification</li>
-                                                    <li>three</li>
-                                                    <li>two</li>
-                                                </ul>
-                                            </FlexView>
-                                            <FlexView column hAlignContent="center">
-                                                <h3>- Colours -</h3>
-                                                <img src={WorldlyColour} style={{width: "25vw"}} alt="WorldlyColour"/>
-                                            </FlexView>
-                                        </ScrollArea>
-                                    </FlexView>
-                                </Slide>
-                            </Slider>
+                        <Slider>
+                            <Slide index={0}>
+                                <p>Page 1</p>
+                            </Slide>
+                            <Slide index={1}>
+                                <FlexView column
+                                    hAlignContent="center"
+                                    height="100vh">
+                                    <ScrollArea className="portfolioBox" horizontal="false">
+                                        <FlexView column hAlignContent="center">
+                                            <img src={WorldlyTitle} style={{ width: "25vw" }} alt="Worldly" />
+                                        </FlexView>
+                                        <FlexView
+                                            column
+                                            hAlignContent="left"
+                                            marginRight="10vw"
+                                            marginLeft="10vw">
+                                            <h2>Main Objective:</h2>
+                                            <p>To design a mobile news application aimed at a younger demographic (age 18 – 25).</p>
+                                            <h2>Tools</h2>
+                                            <ul>
+                                                <li>Adobe Illustrator</li>
+                                                <li>Sketch (MacOS), Lunacy (Windows)</li>
+                                            </ul>
+                                            <h2>Logo</h2>
+                                            <p>A minimalistic earth was designed because it is a unique design (easy to distinguish in a sea of apps) but also very recognisable to most people.</p>
+                                        </FlexView>
+                                        <FlexView column hAlignContent="center">
+                                            <img src={WorldlyComb} style={{ width: "25vw" }} alt="WorldlyComb" />
+                                        </FlexView>
+                                        <FlexView
+                                            column
+                                            hAlignContent="left"
+                                            marginRight="10vw"
+                                            marginLeft="10vw">
+                                            <p>Within the logo and throughout the app I use a blue-green analagous colour scheme because of the relaxing and harmonising effect it creates resulting from it's natural appearence nature. The sans-serif font user helps the user read long bodies of text, a serif font is used for the title to contrast from the main body.</p>
+                                        </FlexView>
+                                        <FlexView column hAlignContent="center">
+                                            <img src={WorldlyColour} style={{ width: "25vw" }} alt="WorldlyColour" />
+                                        </FlexView>
+                                    </ScrollArea>
+                                </FlexView>
+                            </Slide>
+                        </Slider>
                     </SkyLight>
                 </CarouselProvider>
                 <SkyLight
-                        hideOnOverlayClicked ref={ref => this.specThanks = ref}
-                        title="This website would not have been possible without:">
+                    hideOnOverlayClicked ref={ref => this.specThanks = ref}
+                    title="This website would not have been possible without:">
                     <section>
-                        <p>React.js<br/><a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">https://reactjs.org/</a></p>
-                        <p>npm<br/><a href="https://www.npmjs.com/" target="_blank" rel="noopener noreferrer">https://www.npmjs.com/</a></p>
-                        <p>FlexView<br/><a href="https://github.com/buildo/react-flexview" target="_blank" rel="noopener noreferrer">https://github.com/buildo/react-flexview</a></p>
-                        <p>Pure React Carousel<br/><a href="https://www.npmjs.com/package/pure-react-carousel" target="_blank" rel="noopener noreferrer">https://www.npmjs.com/package/pure-react-carousel</a></p>
-                        <p>SkyLight<br/><a href="https://marcio.github.io/react-skylight/" target="_blank" rel="noopener noreferrer">https://marcio.github.io/react-skylight/</a></p>
-                        <p>Scrollbar<br/><a href="https://www.npmjs.com/package/react-scrollbar" target="_blank" rel="noopener noreferrer">https://www.npmjs.com/package/react-scrollbar</a></p>
+                        <p>React.js<br /><a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">https://reactjs.org/</a></p>
+                        <p>npm<br /><a href="https://www.npmjs.com/" target="_blank" rel="noopener noreferrer">https://www.npmjs.com/</a></p>
+                        <p>FlexView<br /><a href="https://github.com/buildo/react-flexview" target="_blank" rel="noopener noreferrer">https://github.com/buildo/react-flexview</a></p>
+                        <p>Pure React Carousel<br /><a href="https://www.npmjs.com/package/pure-react-carousel" target="_blank" rel="noopener noreferrer">https://www.npmjs.com/package/pure-react-carousel</a></p>
+                        <p>SkyLight<br /><a href="https://marcio.github.io/react-skylight/" target="_blank" rel="noopener noreferrer">https://marcio.github.io/react-skylight/</a></p>
+                        <p>Scrollbar<br /><a href="https://www.npmjs.com/package/react-scrollbar" target="_blank" rel="noopener noreferrer">https://www.npmjs.com/package/react-scrollbar</a></p>
                     </section>
                 </SkyLight>
             </div>
